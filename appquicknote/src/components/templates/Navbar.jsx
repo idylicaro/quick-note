a2eb24cf39bc7517d95dc0de82500ce7692f5338
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-05-19 19:59:25 -0300

    [IMP] navBar

--- appquicknote/src/components/templates/Navbar.jsx
+++ appquicknote/src/components/templates/Navbar.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import './sass/Navbar.scss';
 
-export default props =>
-    (
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      
+export default () => (
+  <nav
+    className="navbar is-primary"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="navbar-brand">
+      <a className="navbar-item">
+        <img alt="Logo" />
+      </a>
+      <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+        <span />
+        <span />
+        <span />
+      </span>
     </div>
-    <div class="navbar-menu">
-        <div class="navbar-start">
-            <a class="navbar-item">Home</a>
-        </div>
+    <div className="navbar-menu">
+      <div className="navbar-start">
+        <a className="navbar-item is-active">
+          Home
+        </a>
+        <a className="navbar-item">
+          Workspace
+        </a>
+      </div>
 
-        <div class="navbar-end">
-            <a class="navbar-item">Configuration</a>
-        </div>
+      <div className="navbar-end">
+        <a className="
+        navbar-item"
+        >
+          Configuration
+        </a>
+      </div>
     </div>
   </nav>
-  )
+);
--- appquicknote/src/main/App.jsx
+++ appquicknote/src/main/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from '../components/templates/Navbar.jsx';
 
-export default props => 
+import Navbar from '../components/templates/Navbar';
+
+export default () => (
   <div>
     <Navbar />
   </div>
-
+);
