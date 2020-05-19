a8586540d70f6bfd7efd6be66b2ba1edff4ba4b8
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-05-19 20:53:08 -0300

    [FIX] Format code header file

--- appquicknote/src/components/templates/Header.jsx
+++ appquicknote/src/components/templates/Header.jsx
@@ -4,14 +4,16 @@ import Navbar from './Navbar';
 
 export default () => (
   <section className="hero is-primary">
-      <div className="hero-body">
+    <div className="hero-body">
       <div className="container">
         <h1 className="
         is-size-1
         has-text-light
         has-text-centered
-        ">
-            QuickNote</h1>
+        "
+        >
+          QuickNote
+        </h1>
       </div>
     </div>
     <div className="hero-foot">
