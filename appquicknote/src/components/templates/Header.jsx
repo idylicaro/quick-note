5597aa8d14f26c257644bf163f18cdb2fda9fd27
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-05-19 20:52:05 -0300

    [IMP] Header

---
+++ appquicknote/src/components/templates/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './sass/Header.scss';
+import Navbar from './Navbar';
+
+export default () => (
+  <section className="hero is-primary">
+      <div className="hero-body">
+      <div className="container">
+        <h1 className="
+        is-size-1
+        has-text-light
+        has-text-centered
+        ">
+            QuickNote</h1>
+      </div>
+    </div>
+    <div className="hero-foot">
+      <div className="container">
+        <Navbar />
+      </div>
+    </div>
+  </section>
+);
--- appquicknote/src/main/App.jsx
+++ appquicknote/src/main/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 
-
-import Navbar from '../components/templates/Navbar';
+import Header from '../components/templates/Header';
 
 export default () => (
-  <div>
-    <Navbar />
+  <div className="app">
+    <Header />
   </div>
 );
