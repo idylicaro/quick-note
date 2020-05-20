e36563c6ba9d0ee8da3c8528d9ce63e16d968017
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-20 00:35:01 -0300

    [IMP] Footer

--- appquicknote/src/main/App.jsx
+++ appquicknote/src/main/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Header from '../components/templates/Header';
+import Footer from '../components/templates/Footer';
 
 export default () => (
   <div className="app">
     <Header />
+    <Footer />
   </div>
 );
