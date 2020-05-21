2c6b73237242ff809ea238c0e31e9ab05b5e2eaf
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-21 15:03:37 -0300

    [WIP] content

--- appquicknote/src/main/App.jsx
+++ appquicknote/src/main/App.jsx
@@ -3,10 +3,12 @@ import './App.css';
 
 import Header from '../components/templates/Header';
 import Footer from '../components/templates/Footer';
+import Textarea from '../components/templates/Textarea';
 
 export default () => (
   <div className="app">
     <Header />
+    <Textarea />
     <Footer />
   </div>
 );
