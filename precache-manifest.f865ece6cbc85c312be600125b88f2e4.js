f3ba44794db2bd599e97feb39604764ee690763c
Idyl Icaro dos Santos <idylicaro.se@hotmaill.com>
2020-05-25 22:49:33 -0300

    Updates

---
+++ precache-manifest.f865ece6cbc85c312be600125b88f2e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c3e7ae8dce299a4e14a5356ae93f7b",
+    "url": "/quick-note/index.html"
+  },
+  {
+    "revision": "5750900b800d952d97b2",
+    "url": "/quick-note/static/css/2.f4da3fac.chunk.css"
+  },
+  {
+    "revision": "e054cd2e5fd2401e1420",
+    "url": "/quick-note/static/css/main.17a9d397.chunk.css"
+  },
+  {
+    "revision": "5750900b800d952d97b2",
+    "url": "/quick-note/static/js/2.b9197de2.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/quick-note/static/js/2.b9197de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e054cd2e5fd2401e1420",
+    "url": "/quick-note/static/js/main.939daefa.chunk.js"
+  },
+  {
+    "revision": "8035256dc40e62c95213",
+    "url": "/quick-note/static/js/runtime-main.8c4b3cce.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/quick-note/precache-manifest.f865ece6cbc85c312be600125b88f2e4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quick-note/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
